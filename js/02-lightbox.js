1b04cf64c15e2a588a0982a86140f060b4ab3051
Ihor Borkaliuk <happyviolence1990@gmail.com>
2022-08-09 20:34:45 +0200

    task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
 console.log(galleryItems);
+
+const galleryRef = document.querySelector('.gallery')
+
+const gallery = []
+
+galleryItems.forEach(image => {
+    const galleryLink = document.createElement('a')
+    galleryLink.classList.add('gallery__link')
+    galleryLink.href = image.original
+    const galleryImage = document.createElement('img')
+    galleryImage.classList.add('gallery__image')
+    galleryImage.src = image.preview
+    galleryImage.alt = image.description
+
+    galleryLink.append(galleryImage)
+    gallery.push(galleryLink)
+
+})
+
+galleryRef.append(...gallery)
+
+
+let imgLab = new SimpleLightbox('.gallery a', {
+    captionDelay: '150',
+    animationSpeed: '150'
+});
